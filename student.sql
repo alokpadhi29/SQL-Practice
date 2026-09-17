bb881ab0411bde1013231cf1999d716f3548fa08
Alokpadhi123 <alokpadhi243@gmail.com>
2026-09-17 23:25:19 +0530

    Add student SQL Day 1

---
+++ student.sql
@@ -0,0 +1,14 @@
+mysql> select * from student;
++------+-------------------+----------+------+--------------+--------------+-------------------+------------------+--------+
+| id   | name              | city     | age  | created_time | created_date | Father_name       | mother_name      | graede |
++------+-------------------+----------+------+--------------+--------------+-------------------+------------------+--------+
+|    1 | alok              | bbsr     |   24 | 23:55:45     | 2026-09-17   | ratnakar padhi    | jayanti padhi    | A      |
+|    2 | sai               | cuttack  |   22 | 23:55:45     | 2026-09-17   | maydhare padhi    | jayanti padhi    | A      |
+|    4 | satyabrata        | balesore |   26 | 23:55:45     | 2026-09-17   | sibaji sankar sau | madhumita padhi  | B      |
+|    5 | satyabrata parida | balesore |   26 | 23:55:45     | 2026-09-17   | sibaji sankar sau | madhumita bahara | B      |
+|    6 | sainath parida    | bbsr     |   26 | 23:55:45     | 2026-09-17   | sankar sau        | madhumita bahara | B      |
+|    3 | satya             | bbsr     |   23 | 23:55:45     | 2026-09-17   | sibaji sau        | madhu padhi      | B      |
++------+-------------------+----------+------+--------------+--------------+-------------------+------------------+--------+
+6 rows in set (0.00 sec)
+
+mysql> notee;
